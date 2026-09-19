b4a8cbfd1d175a7f0c965e5ad295a92abf2825a0
orangesobeautiful <orangesobeautiful@gmail.com>
2026-09-19 23:34:45 +0800

    fix: 支援 Hugo 0.166 建置

--- yilai/postcss.config.js
+++ yilai/postcss.config.js
@@ -1,7 +1,21 @@
+const path = require('node:path')
+const fs = require('node:fs')
+
+const importPaths = [
+  path.join(__dirname, 'themes/demius/assets/css'),
+  path.join(__dirname, 'assets/css')
+]
+
 module.exports = {
   plugins: [
     require('postcss-import')({
-      path: ['themes/demius/assets/css', 'assets/css']
+      path: importPaths,
+      resolve(id, basedir) {
+        const candidates = [basedir, ...importPaths]
+          .map(directory => path.resolve(directory, id))
+
+        return candidates.find(file => fs.existsSync(file)) || id
+      }
     })
   ]
 }
